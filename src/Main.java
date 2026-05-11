9e3116cf581f1aed54a1efc66e2517b0fbaa58c5
BOCO SALOMON <boco0075620@univmetiers.ci>
2026-05-11 22:18:15 +0000

    Correction exercice 1

--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,62 @@
 class ContactsManager {
 
-    int friendsCount;
     Contact[] myFriends;
+    int friendsCount;
 
     ContactsManager() {
         this.friendsCount = 0;
         this.myFriends = new Contact[500];
     }
 
-    // Ajoute un contact dans le tableau
     void addContact(Contact contact) {
         myFriends[friendsCount] = contact;
         friendsCount++;
     }
 
-    // Cherche un contact par son nom et le retourner
     Contact searchContact(String searchName) {
         for (int i = 0; i < friendsCount; i++) {
             if (myFriends[i].name.equals(searchName)) {
-                return myFriends[i]; // trouvé !
+                return myFriends[i];
             }
         }
-        return null; // pas trouvé
+        return null;
     }
 }
-
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Créer le gestionnaire
+        // Étape 1 : créer le gestionnaire
         ContactsManager myContactsManager = new ContactsManager();
 
+        Contact contact = new Contact();
+        contact.name = "Alice";
+        contact.phoneNumber = "0102030405";
+        myContactsManager.addContact(contact);
 
-        Contact contact1 = new Contact();
-        contact1.name = "Alice";
-        contact1.phoneNumber = "0102030405";
-        myContactsManager.addContact(contact1);
+        contact = new Contact();
+        contact.name = "Bob";
+        contact.phoneNumber = "0607080910";
+        myContactsManager.addContact(contact);
 
+        contact = new Contact();
+        contact.name = "Rachelle";
+        contact.phoneNumber = "0611223344";
+        myContactsManager.addContact(contact);
 
-        Contact contact2 = new Contact();
-        contact2.name = "Bob";
-        contact2.phoneNumber = "0607080910";
-        myContactsManager.addContact(contact2);
+        contact = new Contact();
+        contact.name = "David";
+        contact.phoneNumber = "0755667788";
+        myContactsManager.addContact(contact);
 
+        contact = new Contact();
+        contact.name = "Ruth";
+        contact.phoneNumber = "0699887766";
+        myContactsManager.addContact(contact);
 
-        Contact contact3 = new Contact();
-        contact3.name = "Chloé";
-        contact3.phoneNumber = "0611223344";
-        myContactsManager.addContact(contact3);
-
-
-        Contact contact4 = new Contact();
-        contact4.name = "David";
-        contact4.phoneNumber = "0755667788";
-        myContactsManager.addContact(contact4);
-
-
-        Contact contact5 = new Contact();
-        contact5.name = "Emma";
-        contact5.phoneNumber = "0699887766";
-        myContactsManager.addContact(contact5);
-
-        // Rechercher un contact et afficher son numéro
-        Contact resultat = myContactsManager.searchContact("Chloé");
-
-        if (resultat != null) {
-            System.out.println("Contact trouvé : " + resultat.name);
-            System.out.println("Numéro de téléphone : " + resultat.phoneNumber);
-        } else {
-            System.out.println("Contact non trouvé.");
-        }
+        // Rechercher et afficher uniquement le numéro
+        Contact resultat = myContactsManager.searchContact("Ruth");
+        System.out.println(resultat.phoneNumber);
     }
 }
